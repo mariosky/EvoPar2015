502fed9fbf58904dd822b7037fe874bdf7992376
mariosky <mariosky@gmail.com>
2014-10-13 13:37:32 -0700

    Added conf example

--- code/start_evospace_in_amazon.py
+++ code/start_evospace_in_amazon.py
@@ -19,7 +19,7 @@ conn = boto.ec2.connect_to_region("us-east-1",
 
 
 reservations = conn.run_instances(
-        'ami-884bf7e0',
+        'ami-28843d40',min_count=2, max_count=2,
         key_name='evospace',
         instance_type='m3.medium',
         security_groups=['launch-wizard-1'])
@@ -35,8 +35,6 @@ for instance in reservations.instances:
     print "running"
 
 
-print reservations.instances[0].id
-print reservations.instances[0].ip_address
 
 
 print "waiting..."
@@ -44,12 +42,23 @@ time.sleep(40)
 
 
 def execute_task():
-    sudo('python evospace/cherry_server.py')
+    sudo('python evospace/evospace_deamon.py')
+
+print "EvoSpace:"
+print reservations.instances[0].public_dns_name
+print reservations.instances[0].id
+print reservations.instances[0].ip_address
+
+print "Redis:"
+print reservations.instances[1].public_dns_name
+print reservations.instances[1].id
+print reservations.instances[1].ip_address
+
 
 env.hosts = [reservations.instances[0].public_dns_name]
 env.user = 'ubuntu'
 env.key_filename = 'evospace.pem'
 
 execute(execute_task)
-conn.terminate_instances(instance_ids=[reservations.instances[0].id])
+#conn.terminate_instances(instance_ids=[reservations.instances[0].id])
 
